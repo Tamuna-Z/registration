03b473a9d2743972ed4f3fd95d0eeabb3b18d803
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-08-02 20:30:49 +0400

    add all inputs validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,53 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
 
-
-
-
-
 function App() {
-  const { register ,handleSubmit,formState:{errors}} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const onSubmitFunc=(data)=>{
+  const onSubmitFunc = (data) => {
     console.log(data);
-  }
-   console.log(errors);
+  };
+  console.log(errors);
   return (
     <>
       <form onSubmit={handleSubmit(onSubmitFunc)}>
         <div>
           <label>Name</label>
-          <input {...register("name")} />
+          <input
+            {...register("name", {
+              pattern: { value: /^[A-Za-z]+$/i, message: "only alphabets" },
+            })}
+          />
+          {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
         </div>
         <div>
-        <label>SurName</label>
-          <input {...register("surName")} />
+          <label>SurName</label>
+          <input {...register("surName",{
+              pattern: { value: /^[A-Za-z]+$/i, message: "only alphabets" },
+            })} />
         </div>
         <div>
-        <label>email</label>
-          <input {...register("email", {required:true})} />
-          {errors.email && <p style={{color:"red"}}>Error happened</p>}
+          <label>email</label>
+          <input
+            {...register("email", {
+              required: "error happened",
+              pattern: {
+                value:
+                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "email not valid",
+              },
+            })}
+          />
+          {errors.email && (
+            <p style={{ color: "red" }}>{errors.email.message}</p>
+          )}
         </div>
         <button type="submit">register</button>
       </form>
-      
     </>
   );
 }
