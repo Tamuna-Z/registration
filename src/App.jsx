f71db53405a0b5627afb2e7a473d412cc4721526
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-08-02 19:50:11 +0400

    add validation to email

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import { useForm } from "react-hook-form";
 
 
 function App() {
-  const { register ,handleSubmit} = useForm();
+  const { register ,handleSubmit,formState:{errors}} = useForm();
 
   const onSubmitFunc=(data)=>{
     console.log(data);
   }
-
+   console.log(errors);
   return (
     <>
       <form onSubmit={handleSubmit(onSubmitFunc)}>
@@ -26,7 +26,8 @@ function App() {
         </div>
         <div>
         <label>email</label>
-          <input {...register("email")} />
+          <input {...register("email", {required:true})} />
+          {errors.email && <p style={{color:"red"}}>Error happened</p>}
         </div>
         <button type="submit">register</button>
       </form>
